ff7e797d3beb4ee7e356b94fd3fee6994f344de7
Eren <erentunc97@gmail.com>
2024-04-22 11:29:04 +0200

    add obsolete test directory

--- domhmm/tests/test_domhmm.py
+++ domhmm/tests/test_domhmm.py
@@ -5,6 +5,7 @@ Unit and regression test for the domhmm package.
 # Import package, test suite, and other packages as needed
 import domhmm
 import sys
+import os
 import MDAnalysis as mda
 
 def test_domhmm_imported():
@@ -14,8 +15,9 @@ def test_domhmm_imported():
 
 def test_run():
     """Demo testing to try run """
-    path2xtc = "domhmm/tests/data/md_center_mol_last2mus.xtc"
-    path2tpr = "domhmm/tests/data/mem.tpr"
+    directory = os.getcwd()
+    path2xtc = f"{directory}/data/md_center_mol_last2mus.xtc"
+    path2tpr = f"{directory}/data/mem.tpr"
     uni = mda.Universe(path2tpr, path2xtc)
 
     # CHOL is out for this run because it doesn't have two tails. Not standard phospholipid
