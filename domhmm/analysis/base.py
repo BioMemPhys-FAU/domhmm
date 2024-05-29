1e17537b8943de4b3f0299907142da1f1c985140
Eren <erentunc97@gmail.com>
2024-05-29 13:49:20 +0200

    bug fix on sterol atom selection for leaflet calculations

--- domhmm/analysis/base.py
+++ domhmm/analysis/base.py
@@ -162,7 +162,7 @@ class LeafletAnalysisBase(AnalysisBase):
 
         for rsn, atoms in self.sterols.items():
             # TODO Find more user-friendly way for sterol atom selection
-            sterol = self.universe.select_atoms(f"name {atoms[0]}")
+            sterol = self.universe.select_atoms(f"resname {rsn} and name {atoms[0]}")
             upper_sterol = distances.distance_array(reference=sterol, configuration=leaflet_selection['0'],
                                                     box=self.universe.trajectory.ts.dimensions)
             lower_sterol = distances.distance_array(reference=sterol, configuration=leaflet_selection['1'],
@@ -244,8 +244,6 @@ class LeafletAnalysisBase(AnalysisBase):
             # Make for every type an extra dictionary
             # Iterate over tails (e.g. for standard phospholipids that 2)
             for i, tail in enumerate(self.tails[resname]):
-                # Check for correct input
-                assert len(tail) % 2 == 0, 'Error: Selection list for tails must be even'
                 # Prepare a MDAnalysis selection string (I.e. ['C22', 'H2R'] -> 'name C22 or name H2R')
                 tail_sele_str = 'name ' + ' or name '.join(tail)
                 # Select for correct lipid type (I.e. I.e. ['C22', 'H2R'] -> 'name C22 or name H2R' ->
